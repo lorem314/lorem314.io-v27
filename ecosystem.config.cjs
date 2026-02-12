3b37b5506b7f384d530a17f2ae1f60215d23b699
lorem314 <dzy314225@gmail.com>
2026-02-12 09:34:41 +0800

    chore: rename ecosystem.config.js
    Rename ecosystem.config.js to ecosystem.config.cjs.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
       error_file: "./logs/error.log",
       out_file: "./logs/out.log",
       log_date_format: "YYYY-MM-DD HH:mm:ss",
+
+      env: {
+        NODE_ENV: "production",
+      },
     },
   ],
 }
